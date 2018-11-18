15c8fa08e21d14e7cb4fc71bb133b00ca19d0dcb
unknown <talha_5881@hotmail.com>
2018-11-19 02:31:42 +0400

    SensorModel no longer has a dependancy

--- models/sensormodel.js
+++ models/sensormodel.js
@@ -5,7 +5,6 @@ var ObjectId = mongoose.Schema.Types.ObjectId; // referencing a schema
 
 var espdata = new Schema({
   // stores the esp32 data
-  userinformation: { type: ObjectId },
   hygrometer: { type: Number, default: 0 },
   waterlevel: { type: Number, default: 0 },
   pH: { type: Number, default: 0.0 }
