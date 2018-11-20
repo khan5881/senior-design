d923ba26cf2eb7d82a4c996824dc04e61353a3c8
Muhammad Khan <b00058197@aus.edu>
2018-11-20 19:32:26 +0400

    Registration and login works

--- Auth/authcontroller.js
+++ Auth/authcontroller.js
@@ -40,4 +40,23 @@ router.get("/me", VerifyToken, function(req, res, next) {
   });
 });
 
+router.post("/login", function(req, res) {
+  User.findOne({ email: req.body.email }, function(err, user) {
+    if (err) return res.status(500).send("Error on the server.");
+    if (!user) return res.status(404).send("No user found.");
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.passwordHash);
+    if (!passwordIsValid)
+      return res.status(401).send({ auth: false, token: null });
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+    // return the information including token as JSON
+    res.status(200).send({ auth: true, token: token });
+  });
+});
+
+router.get('/logout', function(req, res) {
+  res.status(200).send({ auth: false, token: null });
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -2,58 +2,7 @@ var express = require("express");
 var app = express();
 var db = require("./db");
 
-var UserController = require("./user/usercontroller");
-app.use("/users", UserController);
-
 var AuthController = require("./auth/authcontroller");
 app.use("/api/auth", AuthController);
-
-// app.post("/login_user", urlencodedParser, function(req, res) {
-//   var newUser = user;
-//   newUser.find({ email: req.body.email }, function(err, docs) {
-//     if (err) {
-//       // error handling from database side
-//       res.send(err);
-//     } else if (docs.length) {
-//       // in the event that we have recieved a document
-//       if (bcrypt.compareSync(req.body.password, docs[0].passwordHash)) {
-//         res.send("Success");
-//       } else {
-//         res.send("Fail");
-//       }
-//     } else {
-//       res.send("UserNotFound");
-//     }
-//   });
-// });
-
-// app.post("/test", urlencodedParser, function(req, res) {
-// intialize a model, store values in it
-// connect it to the user id
-// store new db in thingy
-
-//   var x = sensor;
-//   var y = new x({
-//     userinformation: newUser._id
-//   });
-// });
-
-// let query = newUser.find({ email: req.body.email });
-//   query
-//     .exec()
-//     .then(function(val) {
-//       if (val[0].passwordHash == req.body.password) {
-//         res.send("Success");
-//       } else {
-//         res.send("Fail");
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.send("Fail", e);
-//     });
-
-//   // res.send(newUser);
-// });
 
 module.exports = app;
