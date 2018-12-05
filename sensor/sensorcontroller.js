997105179152bab66b9024b0fc2c6033d75851c9
unknown <talha_5881@hotmail.com>
2018-12-05 07:32:43 +0400

    added latest hour val

--- sensor/sensorcontroller.js
+++ sensor/sensorcontroller.js
@@ -16,7 +16,7 @@ router.get("/dailyval_one", VerifyToken, function(req, res) {
     if (!user) return res.status(404).send("No user found.");
 
     let x = Sensor.find({ espid: user.espid[0] }, { _id: 0 })
-      .sort({ timestamp: 1 })
+      .sort({ timestamp: -1 })
       .limit(24);
 
     x.exec(function(err, docs) {
@@ -33,8 +33,42 @@ router.get("/dailyval_two", VerifyToken, function(req, res) {
     if (!user) return res.status(404).send("No user found.");
 
     let x = Sensor.find({ espid: user.espid[1] }, { _id: 0 })
-      .sort({ timestamp: 1 })
+      .sort({ timestamp: -1 })
       .limit(24);
+
+    x.exec(function(err, docs) {
+      if (err) console.log(err);
+      res.status(200).send(docs);
+    });
+  });
+});
+
+router.get("/latest_hour1", VerifyToken, function(req, res) {
+  User.findById(req.userId, { passwordHash: 0 }, function(err, user) {
+    if (err)
+      return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+
+    let x = Sensor.find({ espid: user.espid[0] }, { _id: 0 })
+      .sort({ timestamp: -1 })
+      .limit(1);
+
+    x.exec(function(err, docs) {
+      if (err) console.log(err);
+      res.status(200).send(docs);
+    });
+  });
+});
+
+router.get("/latest_hour2", VerifyToken, function(req, res) {
+  User.findById(req.userId, { passwordHash: 0 }, function(err, user) {
+    if (err)
+      return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+
+    let x = Sensor.find({ espid: user.espid[1] }, { _id: 0 })
+      .sort({ timestamp: -1 })
+      .limit(1);
 
     x.exec(function(err, docs) {
       if (err) console.log(err);
